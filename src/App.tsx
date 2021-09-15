f0935653a0100830bc0e2d2e019e617657e680a7
Shen Shuntian <ssthouse@163.com>
2021-09-15 20:33:25 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import "./styles.less";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import DomChart from "./pages/dom-chart";
+import SvgChart from "./pages/svg-chart";
+import CanvasChart from "./pages/canvas-chart";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/dom-chart">DOM</Link>
+            </li>
+            <li>
+              <Link to="/svg-chart">SVG</Link>
+            </li>
+            <li>
+              <Link to="/canvas-chart">CANVAS</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dom-chart">
+            <DomChart />
+          </Route>
+          <Route path="/svg-chart">
+            <SvgChart />
+          </Route>
+          <Route path="/canvas-chart">
+            <CanvasChart />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ src/pages/dom-chart.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DomChart: React.FC = () => {
+  return <div>DOM</div>;
+};
+
+export default DomChart;
